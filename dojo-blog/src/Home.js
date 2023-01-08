097d98c97a4d597d42dcd8ad7a05568630e5eddd
YunJiaChen <B10202015@gmail.com>
2023-01-08 19:24:10 +0800

    lesson#7

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,22 +1,20 @@
-import { useState } from "react";
 const Home = () => {
-    const [name, setName] = useState('Rita');
-    const [age, setAge] = useState('28');
-    const handleClick = () =>{
-        // console.log('hello,Rita',e);
-        setName('Mark');
-        setAge('27');
+    
+    const handleClick = (e) =>{
+        console.log('hello,Rita',e);
+        
     }
-    // const handleClickAgain = (name,e) =>{
-    //     console.log('hello,'+ name,e.target);
-    // } lesson#7 ClickEvent
+    const handleClickAgain = (name,e) =>{
+        console.log('hello,'+ name,e.target);
+    //lesson#7 ClickEvent 
+    } 
 
     return (  
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
+            
             <button onClick={handleClick}>Click me</button>
-            {/* <button onClick={(e) => handleClickAgain('Cindy',e)}>Click me again</button>lesson#7 ClickEvent */}
+            <button onClick={(e) => handleClickAgain('Cindy',e)}>Click me again</button>
         </div>
     );
 }
