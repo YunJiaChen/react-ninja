f182037c73a86105bcfb93d6f0f596cc78ea083e
YunJiaChen <B10202015@gmail.com>
2023-01-15 22:04:19 +0800

    lesson#19-HandingFetchErrors

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,14 +3,23 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error,setError] = useState(null);
     useEffect(() => {
         setTimeout(() =>{
-            fetch('http://localhost:8000/blogs')
+            fetch('http://localhost:8000/blogss')//blogs多加1個s讓載入Data產生錯誤
             .then(res =>{
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
                 return res.json();
             })
             .then(data =>{
                 setBlogs(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch(err =>{
+                setError(err.message);
                 setIsLoading(false);
             })
         },1000);//模擬從遠端server載入資料的時間
@@ -18,6 +27,7 @@ const Home = () => {
     },[]);
     return (  
         <div className="home">
+            {error && <div>{ error }</div>}
             {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title={'All Blog'}/>}
 
