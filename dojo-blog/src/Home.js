6d72e8619e9ca3b8f7b5767b0437f1df359ccfc0
YunJiaChen <B10202015@gmail.com>
2023-01-15 18:49:19 +0800

    lesson#18-ConditionLoadingMessage

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,18 +2,23 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-    
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() =>{
+            fetch('http://localhost:8000/blogs')
             .then(res =>{
                 return res.json();
             })
             .then(data =>{
                 setBlogs(data);
+                setIsLoading(false);
             })
+        },1000);//模擬從遠端server載入資料的時間
+       
     },[]);
     return (  
         <div className="home">
+            {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title={'All Blog'}/>}
 
         </div>
