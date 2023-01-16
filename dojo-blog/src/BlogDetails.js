a55ce774a8e90d0c106cb5c04d18b6b82dc99e2d
YunJiaChen <B10202015@gmail.com>
2023-01-16 18:28:38 +0800

    lesson#26-ReusingCustomHooks

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs/'+ id);
     return (  
         <div className="blogDetails">
-            <h2>Blog Details - { id }</h2>
+            { isLoading && <div>Loading...</div> }
+            { error && <div>{ error }</div>}
+            { blogs && (
+                <article>
+                    <h2>{ blogs.title }</h2>
+                    <p>Written by { blogs.author }</p>
+                    <div>{ blogs.body }</div>
+                </article>
+            )}
         </div>
     );
 }
