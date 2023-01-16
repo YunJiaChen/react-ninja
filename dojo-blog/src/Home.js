4b640ded37956684458a19d67509c6700d1a8bf0
YunJiaChen <B10202015@gmail.com>
2023-01-16 13:09:12 +0800

    lesson#20-MakingACustomHook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,30 +1,8 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error,setError] = useState(null);
-    useEffect(() => {
-        setTimeout(() =>{
-            fetch('http://localhost:8000/blogss')//blogs多加1個s讓載入Data產生錯誤
-            .then(res =>{
-                if(!res.ok) {
-                    throw Error('could not fetch the data for that resource');
-                }
-                return res.json();
-            })
-            .then(data =>{
-                setBlogs(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            .catch(err =>{
-                setError(err.message);
-                setIsLoading(false);
-            })
-        },1000);//模擬從遠端server載入資料的時間
-       
-    },[]);
+    const { data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
     return (  
         <div className="home">
             {error && <div>{ error }</div>}
