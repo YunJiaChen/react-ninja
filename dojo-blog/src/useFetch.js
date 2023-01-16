7dd39713618f6daa21cb5f4acf22d385a0cac72b
YunJiaChen <B10202015@gmail.com>
2023-01-16 16:50:30 +0800

    lesson#24-useEffectCleanup

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -5,8 +5,9 @@ const useFetch = (url) => {
     const [isLoading, setIsLoading] = useState(true);
     const [error,setError] = useState(null);
     useEffect(() => {
+        const abortCont = new AbortController();
         setTimeout(() =>{
-            fetch(url)
+            fetch(url, { signal: abortCont.signal})
             .then(res =>{
                 if(!res.ok) {
                     throw Error('could not fetch the data for that resource');
@@ -19,11 +20,17 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err =>{
-                setError(err.message);
-                setIsLoading(false);
+                if (err.name === 'AbortError'){
+                    console.log('fetch aborted');
+                }
+                else{
+                    setError(err.message);
+                    setIsLoading(false);
+                }    
             })
         },1000);//模擬從遠端server載入資料的時間
-       
+
+       return () => abortCont.abort();
     },[url]);
 
     return { data, isLoading, error};
