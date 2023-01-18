6d9ceb363fa7565cc1bfbf438855258d35354fde
YunJiaChen <B10202015@gmail.com>
2023-01-18 20:12:40 +0800

    lesson#31-DeletingBlogs

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs/'+ id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blogs.id,{
+            method: "DELETE"
+        })
+        .then(() => {
+            history.push('/');
+        })
+    }
     return (  
         <div className="blogDetails">
             { isLoading && <div>Loading...</div> }
@@ -13,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{ blogs.title }</h2>
                     <p>Written by { blogs.author }</p>
                     <div>{ blogs.body }</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
